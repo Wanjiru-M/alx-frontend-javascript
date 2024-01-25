c9f51dce2546777d302d6b4a03cf4f36d28d5185
wanjirumephis <wanjirumephis@gmail.com>
2024-01-25 01:23:40 -0800

    Update

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,7 @@
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
-    firstName,
-    lastName,
+    firstName: `${firstName}`,
+    lastName: `${lastName}`,
   });
 }
 
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  let result;
-  try {
-    result = mathFunction();
-  } catch (error) {
-    result = `Error: ${error.message}`;
-  }
-  queue.push(result);
-  queue.push('Guardrail was processed');
 
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
   return queue;
 }
-
